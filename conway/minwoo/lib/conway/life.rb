55e8504dd7eddbdc6577667f6d53c5bf7e91be62
Minwoo Lee <ermaker@gmail.com>
2015-09-28 17:30:20 +0900

    Add #next_generation

--- conway/minwoo/lib/conway/life.rb
+++ conway/minwoo/lib/conway/life.rb
@@ -2,6 +2,67 @@ module Conway
   class Life
     def initialize(array)
       @array = array
+      @boundary = [@array.size, @array.first.size]
+    end
+
+    def normalize(*position)
+      position.zip(@boundary).map do |p, b|
+        if p >= b
+          p - b
+        else
+          p
+        end
+      end
+    end
+
+    def value(*position)
+      @array[position[0]][position[1]]
+    end
+
+    NEIGHBOURS = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1]
+    ]
+
+    def neighbours(*position)
+      neighbours_positions = NEIGHBOURS.map do |n|
+        normalize(*n.zip(position).map { |n_, p_| n_ + p_ })
+      end
+      neighbours_positions.map { |n| value(*n) }
+    end
+
+    def sum(*position)
+      neighbours(*position).reduce(:+)
+    end
+
+    DEAD_COND = [3]
+    ALIVE_COND = [2, 3]
+
+    def next_value(*position)
+      cond =
+        case value(*position)
+        when 0
+          DEAD_COND
+        when 1
+          ALIVE_COND
+        else
+          fail
+        end
+      cond.include?(sum(*position)) ? 1 : 0
+    end
+
+    def next_generation
+      @array.map.with_index do |line, line_index|
+        line.map.with_index do |_cell, cell_index|
+          next_value(line_index, cell_index)
+        end
+      end
     end
   end
 end
--- conway/minwoo/spec/lib/conway/life_spec.rb
+++ conway/minwoo/spec/lib/conway/life_spec.rb
@@ -4,4 +4,101 @@ RSpec.describe Conway::Life do
   it '#new works' do
     described_class.new([[1, 0, 1], [0, 0, 0], [1, 0, 1]])
   end
+
+  describe 'with basic case' do
+    subject do
+      described_class.new([[1, 0, 1], [0, 0, 0], [1, 0, 1]])
+    end
+
+    it '#normalize works' do
+      expect(subject.normalize(0, 0)).to eq([0, 0])
+      expect(subject.normalize(3, 3)).to eq([0, 0])
+    end
+
+    it '#neighbours works' do
+      expect(subject.neighbours(1, 1)).to eq([1, 0, 1, 0, 0, 1, 0, 1])
+    end
+
+    it '#sum works' do
+      expect(subject.sum(1, 1)).to eq(4)
+    end
+
+    it '#next_value works' do
+      allow(subject).to receive(:value).with(1, 1).and_return(0)
+      expect(subject).to receive(:sum).with(1, 1).and_return(3)
+      expect(subject.next_value(1, 1)).to eq(1)
+      expect(subject).to receive(:sum).with(1, 1).and_return(2)
+      expect(subject.next_value(1, 1)).to eq(0)
+
+      allow(subject).to receive(:value).with(1, 1).and_return(1)
+      expect(subject).to receive(:sum).with(1, 1).and_return(3)
+      expect(subject.next_value(1, 1)).to eq(1)
+      expect(subject).to receive(:sum).with(1, 1).and_return(2)
+      expect(subject.next_value(1, 1)).to eq(1)
+      expect(subject).to receive(:sum).with(1, 1).and_return(1)
+      expect(subject.next_value(1, 1)).to eq(0)
+    end
+
+    it '#next_generation works' do
+      expect(subject.next_generation).to eq([[1, 0, 1], [0, 0, 0], [1, 0, 1]])
+    end
+  end
+
+  it '#next_generation works' do
+    # zero
+    sub = described_class.new([[1, 0, 1], [0, 0, 0], [1, 0, 1]])
+    expect(sub.next_generation).to eq([[1, 0, 1], [0, 0, 0], [1, 0, 1]])
+
+    # case 1
+    sub = described_class.new([[1, 0, 1], [0, 0, 1], [1, 0, 0]])
+    expect(sub.next_generation).to eq([[1, 0, 1], [0, 0, 1], [1, 0, 0]])
+
+    # rubocop:disable Metrics/LineLength
+
+    # case 2
+    sub = described_class.new(
+      [[1, 0, 0, 1, 1], [0, 1, 0, 0, 0], [1, 0, 1, 1, 0], [0, 0, 0, 1, 0], [0, 1, 1, 1, 0]]
+    )
+    expect(sub.next_generation).to eq(
+      [[1, 0, 0, 1, 1], [0, 1, 0, 0, 0], [0, 1, 1, 1, 1], [0, 0, 0, 0, 0], [1, 1, 0, 0, 0]]
+    )
+    sub = described_class.new(
+      [[1, 0, 0, 1, 1], [0, 1, 0, 0, 0], [0, 1, 1, 1, 1], [0, 0, 0, 0, 0], [1, 1, 0, 0, 0]]
+    )
+    expect(sub.next_generation).to eq(
+      [[0, 0, 1, 0, 1], [0, 1, 0, 0, 0], [1, 1, 1, 1, 0], [0, 0, 0, 1, 1], [1, 1, 0, 0, 0]]
+    )
+    sub = described_class.new(
+      [[0, 0, 1, 0, 1], [0, 1, 0, 0, 0], [1, 1, 1, 1, 0], [0, 0, 0, 1, 1], [1, 1, 0, 0, 0]]
+    )
+    expect(sub.next_generation).to eq(
+      [[0, 0, 1, 0, 0], [0, 0, 0, 0, 1], [1, 1, 0, 1, 0], [0, 0, 0, 1, 0], [1, 1, 1, 0, 0]]
+    )
+    sub = described_class.new(
+      [[0, 0, 1, 0, 0], [0, 0, 0, 0, 1], [1, 1, 0, 1, 0], [0, 0, 0, 1, 0], [1, 1, 1, 0, 0]]
+    )
+    expect(sub.next_generation).to eq(
+      [[1, 0, 1, 1, 0], [1, 1, 1, 1, 1], [1, 0, 1, 1, 0], [0, 0, 0, 1, 0], [0, 1, 1, 1, 0]]
+    )
+    sub = described_class.new(
+      [[1, 0, 1, 1, 0], [1, 1, 1, 1, 1], [1, 0, 1, 1, 0], [0, 0, 0, 1, 0], [0, 1, 1, 1, 0]]
+    )
+    expect(sub.next_generation).to eq(
+      [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 0, 0, 0]]
+    )
+    sub = described_class.new(
+      [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 0, 0, 0]]
+    )
+    expect(sub.next_generation).to eq(
+      [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+    )
+    sub = described_class.new(
+      [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 0, 0, 0]]
+    )
+    expect(sub.next_generation).to eq(
+      [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+    )
+
+    # rubocop:enable Metrics/LineLength
+  end
 end
